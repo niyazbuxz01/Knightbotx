1788e86b03f6f29598fa796d51c679d12263982c
niyazbuxz01 <niyazans056@gmail.com>
2025-02-06 11:02:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'kaiser',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "kaiser buxz",
+  botOwner: 'Nijju', // Your name
+  ownerNumber: '918700292512', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
